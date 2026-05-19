38193c3a9764d8c70103382b04da0c14bf83b48b
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-19 13:49:02 +0200

    Implement canUnlockAll function to determine if all boxes can be unlocked

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env pyhton3
+def canUnlockAll(boxes):
+    n = len(boxes)
+    keychain = [0]
+    while True:
+        size_keychain_before = len(keychain)
+        for j in range(len(boxes)):
+            if j in keychain:
+                for key in boxes[j]:
+                    if key not in keychain:
+                        keychain.append(key)
+        size_keychain_after = len(keychain)
+        if size_keychain_before == size_keychain_after:
+            break
+    for i in range(0, n - 1):
+        if i not in keychain:
+            return False
+    return True
