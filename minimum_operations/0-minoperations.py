a8bc0528a6374332b2deefeb3f02a84f647aeb2f
nohamoulma-hub <noham.oulma@gmail.com>
2026-05-28 19:19:12 +0200

    Add minOperations function to calculate the fewest number of operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Module who calculates the fewest number of operations """
+
+
+def minOperations(n):
+
+    operation = 0
+    diviseur = 2
+
+    if n <= 1:
+        return 0
+
+    while n > 1:
+        if n % diviseur == 0:
+            operation += diviseur
+            n = n // diviseur
+        else:
+            diviseur += 1
+    return operation
