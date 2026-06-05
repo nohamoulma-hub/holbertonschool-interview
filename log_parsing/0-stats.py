11efb46a7c8767d0ad341700857aa1e46fa435cc
nohamoulma-hub <noham.oulma@gmail.com>
2026-06-05 14:02:35 +0200

    Refactor docstring and improve code formatting in 0-stats.py

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" """
+""" Module log parsing"""
 import sys
+
 
 def print_stats(total_size, status_code):
     print("File size: {}".format(total_size))
     for key, value in status_code.items():
         if value > 0:
             print("{}: {}".format(key, value))
-    
 
 
 if __name__ == "__main__":
 
     total_size = 0
-    status_code = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+    status_code = {
+        "200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+        "404": 0, "405": 0, "500": 0}
     counter = 0
 
     try:
@@ -22,11 +24,13 @@ if __name__ == "__main__":
                 counter += 1
                 parts = line.split()
                 total_size += int(parts[8])
-                if parts[7] in status_code:     # "in" pour savoir si parts[6] existe dans status_code
-                    status_code[parts[7]] += 1  # on accède à la valeur via la clé
+                # "in" pour savoir si parts[6] existe dans status_code
+                if parts[7] in status_code:
+                    # on accède à la valeur via la clé
+                    status_code[parts[7]] += 1
                 if counter % 10 == 0:
                     print_stats(total_size, status_code)
-            except:
+            except (IndexError, ValueError):
                 continue
     except KeyboardInterrupt:
-        print_stats(total_size,status_code)
+        print_stats(total_size, status_code)
