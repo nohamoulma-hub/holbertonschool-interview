c293845b96583208be7a2c939d676e672915dac8
nohamoulma-hub <noham.oulma@gmail.com>
2026-06-05 14:10:23 +0200

    Fix print_stats call to ensure final statistics are displayed on completion

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -32,5 +32,7 @@ if __name__ == "__main__":
                     print_stats(total_size, status_code)
             except (IndexError, ValueError):
                 continue
+        print_stats(total_size, status_code)
     except KeyboardInterrupt:
         print_stats(total_size, status_code)
+
