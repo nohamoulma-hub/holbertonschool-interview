c449e5e16edd0d851146df7952fccca1d5b9f108
nohamoulma-hub <noham.oulma@gmail.com>
2026-07-07 13:39:51 +0200

    Add island_perimeter function to calculate the perimeter of an island in a grid

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Island Perimeter """
+
+
+def island_perimeter(grid):
+    perimeter = 0
+    rows = len(grid)
+    columns = len(grid[0])
+
+    for row in range(rows):
+        for column in range(columns):
+            if grid[row][column] == 1:
+                # Check la case du dessus
+                if row == 0 or grid[row - 1][column] == 0:
+                    perimeter += 1
+                # Check la case du bas
+                if row == rows - 1 or grid[row + 1][column] == 0:
+                    perimeter += 1
+                # Check la case de gauche
+                if column == 0 or grid[row][column - 1] == 0:
+                    perimeter += 1
+                # Check la case de droite
+                if column == columns - 1 or grid[row][column + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
