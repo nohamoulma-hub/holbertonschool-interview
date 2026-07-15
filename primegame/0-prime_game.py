fd33d02bb154bb448b87da2969305c404376abc1
nohamoulma-hub <noham.oulma@gmail.com>
2026-07-15 12:17:17 +0200

    Add isWinner function to determine the winner of the Prime Game

---
+++ primegame/0-prime_game.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Module Prime Game"""
+
+
+def isWinner(x, nums):
+    maria_wins = 0
+    ben_wins = 0
+    for n in nums:
+        if n < 2:
+            ben_wins += 1
+        else:
+            is_prime = [True] * (n + 1)
+            is_prime[0] = False
+            is_prime[1] = False
+            for i in range(2, n + 1):
+                if is_prime[i]:
+                    for multiple in range(2*i, n + 1, i):
+                        is_prime[multiple] = False
+            nb_premiers = is_prime.count(True)
+            if nb_premiers % 2 == 1:
+                maria_wins += 1
+            else:
+                ben_wins += 1
+    if maria_wins > ben_wins:
+        return "Maria"
+    if ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
