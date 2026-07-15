207dbd33fee30032e1b7efd04a90c17b53f0b271
nohamoulma-hub <noham.oulma@gmail.com>
2026-07-15 12:20:38 +0200

    Improve docstring in isWinner function for clarity

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -3,8 +3,10 @@
 
 
 def isWinner(x, nums):
+    """ function who determinate the winner of the game """
     maria_wins = 0
     ben_wins = 0
+
     for n in nums:
         if n < 2:
             ben_wins += 1
@@ -12,15 +14,20 @@ def isWinner(x, nums):
             is_prime = [True] * (n + 1)
             is_prime[0] = False
             is_prime[1] = False
+
             for i in range(2, n + 1):
                 if is_prime[i]:
+
                     for multiple in range(2*i, n + 1, i):
                         is_prime[multiple] = False
+
             nb_premiers = is_prime.count(True)
+
             if nb_premiers % 2 == 1:
                 maria_wins += 1
             else:
                 ben_wins += 1
+
     if maria_wins > ben_wins:
         return "Maria"
     if ben_wins > maria_wins:
